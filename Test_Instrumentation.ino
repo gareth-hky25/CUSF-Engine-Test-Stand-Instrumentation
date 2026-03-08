2be84e6982b3c8eace4c9beaee49888a59d0fa46
Ieuan1240 <ijp34@cam.ac.uk>
2026-03-08 15:14:23 +0000

    Set up pins

--- Test_Instrumentation.ino
+++ Test_Instrumentation.ino
@@ -1,6 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+#define PWM1 1
+#define PWM2 2
+#define PWM3 3
+#define PWM4 4
+const int pwmPins[] = {PWM1, PWM2, PWM3, PWM4};
+const int pwmChannels[] = {0, 1, 2, 3};
+const int pwmFrequency = 5000;
 
+void setup() {
+    //ties channels to pins
+    for (int i = 0; i < 4; i++) {
+        ledcSetup(pwmChannels[i], pwmFrequency, 8);
+        ledcAttachPin(pwmPins[i], pwmChannels[i]);
+        ledcWrite(pwmChannels[i], 128); // Set duty cycle to 50%
+    }
 }
 
 void loop() {
